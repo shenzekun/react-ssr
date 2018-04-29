6b746b2a388dc41fa3cb33d52b0512395823b12e
shenzekun <726664809@qq.com>
2018-04-29 23:14:30 +0800

    测试

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { AppContainer } from 'react-hot-loader'; // eslint-disable-line
+// import { AppContainer } from 'react-hot-loader'; // eslint-disable-line
 import App from './App.jsx';
 
 const root = document.getElementById('root');
@@ -16,7 +16,7 @@ render(App);
 
 if (module.hot) {
   module.hot.accept('./App.jsx', () => {
-    const NextApp = require('./App.jsx').default;
+    const NextApp = require('./App.jsx').default; // eslint-disable-line
     render(NextApp);
   });
 }
